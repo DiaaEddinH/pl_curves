83080515240f7e98c33201b718f44426482f4221
DiaaEddinH <diaaeddinh@gmail.com>
2024-04-18 15:53:36 +0100

    Implemented tests for check_columns and gini

--- tests/test_check_columns.py
+++ tests/test_check_columns.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 from pl_curve import check_columns
 import pandas
+import pytest
 
-
-def test_check_columns_correct():
+@pytest.mark.parametrize('dataframe', [pandas.DataFrame([1.0, 0, 0, 0]), 
+pandas.DataFrame([0.1, 0.4, 0.2, 0.3])])
+def test_check_columns_correct(dataframe):
     '''FIXME: Implement this test
     Tests check_columns correctly checks items summing to 1.0 returns true
     '''
-    raise NotImplementedError("Please implement this test")
+    assert check_columns(dataframe) is True
+    #raise NotImplementedError("Please implement this test")
 
 def test_check_columns_incorrect():
     df = pandas.DataFrame([0.1, 0.8])
